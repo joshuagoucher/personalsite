bca5de36d76ad4e713bb6f09ba57f6faef695805
josh <joshuagoucher1@live.co.uk>
2014-02-28 17:09:20 +0000

    added static pages controller, deleted index.html, added bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Personalsite::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/projects"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
